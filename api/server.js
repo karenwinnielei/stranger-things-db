4f21b820acc52904279f61505029df3e4b10d814
Karen Lei <karenwinnielei@gmail.com>
2020-07-16 14:45:55 -0700

    / endpoint running

--- api/server.js
+++ api/server.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const helmet = require('helmet')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const server = express()
+const server = express();
 
-server.use(helmet())
-server.use(express.json())
-server.use(morgan('dev'))
-server.use(cors())
+server.use(helmet());
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(cors());
 
-module.exports = server
+server.get('/', (req, res) => {
+  data = {
+    message: 'Server is running on /',
+    reply: 'Way cool tool!'
+  }
+  res.send(`${data.message} and ${data.reply}`);
+});
+
+module.exports = server;
